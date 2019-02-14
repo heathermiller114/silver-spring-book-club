e8eaec4ef90dd5cc5b5a95eb8c65f815d57762cf
Heather Miller <heather.miller114@gmail.com>
2019-02-14 18:54:02 -0500

    adds logic to get and post /signup routes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,14 +1,25 @@
 class MembersController < ApplicationController
     get '/signup' do
-        erb :create_member
+        if !logged_in?
+            erb :'members/create_member'
+        end
     end
 
     post '/signup' do
+        if params[:email] == "" || params[:password] == ""
+            redirect "/signup"
+        else
+            @member = Member.new(email: params[:email], password: params[:password])
+            @member.save
+            session[:member_id] = @member.id
+
+            #redirect
+        end
         
     end
 
     get '/login' do
-        erb :login
+        erb :'members/login'
     end
 
     post '/login' do
