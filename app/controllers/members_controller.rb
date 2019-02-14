b212a432e999142e5092214f1c8700b00380dc8f
Heather Miller <heather.miller114@gmail.com>
2019-02-14 17:41:40 -0500

    adds routes to members_controller

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,21 @@
+class MembersController < ApplicationController
+    get '/signup' do
+
+    end
+
+    post '/signup' do
+        
+    end
+
+    get '/login' do
+        
+    end
+
+    post '/login' do
+
+    end
+
+    get '/logout' do
+        
+    end
+end
