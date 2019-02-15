1ee80e402e3e371fa13cbc3bef395e551b3cfc05
Heather Miller <heather.miller114@gmail.com>
2019-02-14 20:27:08 -0500

    adds outline of CRUD routes for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,33 @@
 class ReviewsController < ApplicationController
-    
+    get '/reviews' do
+
+    end
+
+    #Create
+    get '/reviews/new' do
+
+    end
+
+    post '/reviews' do
+
+    end
+
+    #Read
+    get '/reviews/:id' do
+
+    end
+
+    #Update
+    get '/reviews/:id/edit' do
+
+    end
+
+    patch '/reviews/:id' do
+
+    end
+
+    #Delete
+    delete '/reviews/:id/delete' do
+        
+    end
 end
