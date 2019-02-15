817603d554cf5b3a121e381253812ae3c26f1e03
Heather Miller <heather.miller114@gmail.com>
2019-02-15 14:48:00 -0500

    adds validations to email and password

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,8 @@ class Member < ActiveRecord::Base
     has_many :books
     has_many :reviews, through: :books
     has_secure_password
+
+    validates :email, :password, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 8 }
 end
