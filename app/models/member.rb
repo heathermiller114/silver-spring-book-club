b701e3dc27965403cfe0a1c33203baf2bdb31194
Heather Miller <heather.miller114@gmail.com>
2019-02-20 17:38:07 -0500

    adds spacing

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,7 +7,7 @@ class Member < ActiveRecord::Base
     has_many :reviews, through: :books
     has_secure_password
 
-    validates :email, :password, presence: true
+    validates :email, presence: true
     validates :email, uniqueness: true
     validates :password, length: { minimum: 8 }
 end
