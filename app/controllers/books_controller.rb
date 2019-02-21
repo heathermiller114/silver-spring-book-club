6bcada685384549485111d5219a386c9ded6529e
Heather Miller <heather.miller114@gmail.com>
2019-02-21 13:18:34 -0500

    adds books/edit route and view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,8 +18,8 @@ class BooksController < ApplicationController
     end
 
     post '/books' do
-        #binding.pry
-        if params[:title].empty?
+        binding.pry
+        if params[:title].empty? || params[:author].empty? || params[:page_count].empty? || params[:plot].empty? || params[:genre].empty?
             redirect "/books/new"
         else
             @book = Book.create(params)
@@ -42,7 +42,12 @@ class BooksController < ApplicationController
 
     #Update
     get '/books/:slug/edit' do
-
+        @book = Book.find_by_slug(params[:slug])
+        if logged_in? && @book && @book.member_id == current_member.id
+            erb :'books/edit_book'
+        else
+            redirect '/login'
+        end
     end
 
     patch '/books/:slug' do
